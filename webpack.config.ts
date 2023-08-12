7834f74ad58600029c08e5fa1cd87b032d331322
shagi9 <shagoferov@gmail.com>
2023-08-12 19:06:39 +0300

    added webpack-dev-server. Added env variables.

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,21 +2,25 @@ import webpack from 'webpack';
 import path from 'path';
 
 import { buildWebpackConfig } from './config/build/buildWebpackConfig';
-import { BuildPaths } from './config/build/types/config';
+import { BuildEnv, BuildPaths } from './config/build/types/config';
 
-const paths: BuildPaths = {
-  entry: path.resolve(__dirname, 'src', 'index.ts'),
-  build: path.join(__dirname, 'build'),
-  html: path.resolve(__dirname, 'public', 'index.html')
-}
+export default (env: BuildEnv) => {
+  const paths: BuildPaths = {
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    build: path.join(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html')
+  }
 
-const mode = 'development';
-const isDev = mode === 'development';
+  const mode = env.mode || 'development';
+  const isDev = mode === 'development';
+  const port = env.port || 3000;
 
-const config: webpack.Configuration = buildWebpackConfig({
-  mode: 'development',
-  paths: paths,
-  isDev
-})
+  const config: webpack.Configuration = buildWebpackConfig({
+    mode: mode,
+    paths: paths,
+    isDev,
+    port: port
+  })
 
-export default config;
+  return config;
+};

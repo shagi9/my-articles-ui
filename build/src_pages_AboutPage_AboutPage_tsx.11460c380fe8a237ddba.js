7c22aaeb95bc20fdd7e13c21d433fc959ca6b46e
shagi9 <shagoferov@gmail.com>
2023-08-20 12:55:10 +0300

    set up routes with lazy loading

---
+++ build/src_pages_AboutPage_AboutPage_tsx.11460c380fe8a237ddba.js
@@ -0,0 +1,25 @@
+"use strict";
+(self["webpackChunkmy_articles"] = self["webpackChunkmy_articles"] || []).push([["src_pages_AboutPage_AboutPage_tsx"],{
+
+/***/ "./src/pages/AboutPage/AboutPage.tsx":
+/*!*******************************************!*\
+  !*** ./src/pages/AboutPage/AboutPage.tsx ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+var AboutPage = function () {
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "About Page" }));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AboutPage);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX0Fib3V0UGFnZV9BYm91dFBhZ2VfdHN4LjExNDYwYzM4MGZlOGEyMzdkZGJhLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQWdEO0FBQ2hEO0FBQ0EsWUFBWSxzREFBSSxVQUFVLHdCQUF3QjtBQUNsRDtBQUNBLGlFQUFlLFNBQVMsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL215LWFydGljbGVzLy4vc3JjL3BhZ2VzL0Fib3V0UGFnZS9BYm91dFBhZ2UudHN4Il0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IGpzeCBhcyBfanN4IH0gZnJvbSBcInJlYWN0L2pzeC1ydW50aW1lXCI7XG52YXIgQWJvdXRQYWdlID0gZnVuY3Rpb24gKCkge1xuICAgIHJldHVybiAoX2pzeChcImRpdlwiLCB7IGNoaWxkcmVuOiBcIkFib3V0IFBhZ2VcIiB9KSk7XG59O1xuZXhwb3J0IGRlZmF1bHQgQWJvdXRQYWdlO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from "react";
-import { Counter } from "./components/Counter";
+import React, { Suspense } from "react";
+import { Link, Route, Routes } from 'react-router-dom';
 import './index.scss';
 
+import { Counter } from "./components/Counter";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+
 export const App = () => {
-    return (
-        <div className="app">
-            Test
-            <Counter />
-        </div>
-    )
+  return (
+  	<div className="app">
+			<Link to={'/'}>Main Page</Link> 
+			<Link to={'/about'}>About Page</Link> 
+			<Suspense fallback={<div>Loading...</div>}>
+				<Routes>
+					<Route path={'/'} element={<MainPageAsync />} />
+					<Route path={'/about'} element={<AboutPageAsync />}/>
+				</Routes>
+      </Suspense>
+    </div>
+  )
 }
